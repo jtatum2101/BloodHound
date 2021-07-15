7356693d7249ff620b49a7c108c9cd782ea44f32
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-15 13:22:48 -0500

    email is confirmed!

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "you are signed in successfully!";
+include_once('header.php');
+include_once('database.php');
+session_start();
+
+if($_POST){
+
+    if($validLoginCredentials){
+        $_SESSION['user'] = array(
+            'email' => $email,
+            'login' => 'login',
+            'psw' => $psw,
+        );
+    }   
+}
+if(isset($_SESSION['email'])){
+        echo 'you are logged in successfully, ' .  $_SESSION['email'];
+    } else{
+        echo 'Logged in was not completed! Try again.';
+    }
 
 ?>
