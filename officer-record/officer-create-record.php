63ffd04cce471256d96d366a24fd2a2b91c39a06
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-20 11:06:14 -0500

    got form completed, just need to connect it to the database.

--- officer-record/officer-create-record.php
+++ officer-record/officer-create-record.php
@@ -1,48 +1,54 @@
 <?php
-
-
 session_start();
 if($_POST){
     include 'database.php';
     include 'config.php';
-    $query = "INSERT INTO records (mugshot, criminal_name, criminal_birth_date, criminal_weight, criminal_height, criminal_eye_color, criminal_hair_color, criminal_ethnicity, criminal_charges, criminal_date_of_arrest, criminal_county_of_arrest, author_of_record) VALUES ( :mugshot, :criminal_name, :criminal_birth_date, :criminal_weight, :criminal_height, :criminal_eye_color, :criminal_hair_color, :criminal_ethnicity, :criminal_charges, :criminal_date_of_arrest, :criminal_county_of_arrest, :author_of_record)";
-    $stmt = $con->prepare($query);
+    try{
+        $query = "INSERT INTO records (mugshot, criminal_name, criminal_birth_date, criminal_weight, criminal_height, criminal_eye_color, criminal_hair_color, criminal_ethnicity, criminal_charges, criminal_date_of_arrest, criminal_county_of_arrest, author_of_record) VALUES ( :mugshot, :criminal_name, :criminal_birth_date, :criminal_weight, :criminal_height, :criminal_eye_color, :criminal_hair_color, :criminal_ethnicity, :criminal_charges, :criminal_date_of_arrest, :criminal_county_of_arrest, :author_of_record)";
+        $stmt = $con->prepare($query);
 
 
-    $mugshot=htmlspecialchars(strip_tags($_POST['mugshot']));
-    $criminal_name=htmlspecialchars(strip_tags($_POST['criminal_name']));
-    $criminal_birth_date=htmlspecialchars(strip_tags($_POST['criminal_birth_date']));
-    $criminal_weight=htmlspecialchars(strip_tags($_POST['criminal_weight']));
-    $criminal_height=htmlspecialchars(strip_tags($_POST['criminal_height']));
-    $criminal_eye_color=htmlspecialchars(strip_tags($_POST['criminal_eye_color']));
-    $criminal_hair_color=htmlspecialchars(strip_tags($_POST['criminal_hair_color']));
-    $criminal_ethnicity=htmlspecialchars(strip_tags($_POST['criminal_ethnicity']));
-    $criminal_charges=htmlspecialchars(strip_tags($_POST['criminal_charges']));
-    $criminal_date_of_arrest=htmlspecialchars(strip_tags($_POST['criminal_date_of_arrest']));
-    $criminal_county_of_arrest=htmlspecialchars(strip_tags($_POST['criminal_county_of_arrest']));
-    $author_of_record=htmlspecialchars(strip_tags($_POST['author_of_record']));
+        $mugshot=htmlspecialchars(strip_tags($_POST['mugshot']));
+        $criminal_name=htmlspecialchars(strip_tags($_POST['criminal_name']));
+        $criminal_birth_date=htmlspecialchars(strip_tags($_POST['criminal_birth_date']));
+        $criminal_weight=htmlspecialchars(strip_tags($_POST['criminal_weight']));
+        $criminal_height=htmlspecialchars(strip_tags($_POST['criminal_height']));
+        $criminal_eye_color=htmlspecialchars(strip_tags($_POST['criminal_eye_color']));
+        $criminal_hair_color=htmlspecialchars(strip_tags($_POST['criminal_hair_color']));
+        $criminal_ethnicity=htmlspecialchars(strip_tags($_POST['criminal_ethnicity']));
+        $criminal_charges=htmlspecialchars(strip_tags($_POST['criminal_charges']));
+        $criminal_date_of_arrest=htmlspecialchars(strip_tags($_POST['criminal_date_of_arrest']));
+        $criminal_county_of_arrest=htmlspecialchars(strip_tags($_POST['criminal_county_of_arrest']));
+        $author_of_record=htmlspecialchars(strip_tags($_POST['author_of_record']));
 
 
-    $mugshot = (empty($mugshot)) ? NULL : $mugshot;
+        $mugshot = (empty($mugshot)) ? NULL : $mugshot;
 
-    $stmt->bindParam(':criminal_name', $criminal_name);
-    $criminal_birth_date= date('Y-m-d');
-    $stmt->bindParam(':criminal_birth_date', $criminal_birth_date);
-    $stmt->bindParam(':criminal_weight', $criminal_weight);
-    $stmt->bindParam(':criminal_height', $criminal_height);
-    $stmt->bindParam(':criminal_eye_color', $criminal_eye_color);
-    $stmt->bindParam(':criminal_hair_color', $criminal_hair_color);
-    $stmt->bindParam(':criminal_ethnicity', $criminal_ethnicity);
-    $stmt->bindParam(':criminal_charges', $criminal_charges);
-    $criminal_date_of_arrest=date('Y-m-d');
-    $stmt->bindParam(':criminal_date_of_arrest', $criminal_date_of_arrest);
-    $stmt->bindParam(':criminal_county_of_arrest', $criminal_county_of_arrest);
-    $stmt->bindParam(':author_of_record', $author_of_record);
-    if($stmt->execute()){
-        echo 'Post completed!';
-    }else{
-        echo 'Try again!' . $stmt->errorInfo()[2];
+        $stmt->bindParam(':criminal_name', $criminal_name);
+        $criminal_birth_date= date('Y-m-d');
+        $stmt->bindParam(':criminal_birth_date', $criminal_birth_date);
+        $stmt->bindParam(':criminal_weight', $criminal_weight);
+        $stmt->bindParam(':criminal_height', $criminal_height);
+        $stmt->bindParam(':criminal_eye_color', $criminal_eye_color);
+        $stmt->bindParam(':criminal_hair_color', $criminal_hair_color);
+        $stmt->bindParam(':criminal_ethnicity', $criminal_ethnicity);
+        $stmt->bindParam(':criminal_charges', $criminal_charges);
+        $criminal_date_of_arrest=date('Y-m-d');
+        $stmt->bindParam(':criminal_date_of_arrest', $criminal_date_of_arrest);
+        $stmt->bindParam(':criminal_county_of_arrest', $criminal_county_of_arrest);
+        $stmt->bindParam(':author_of_record', $author_of_record);
+        if($stmt->execute()){
+            header("Location: officer-record/officer-view-my-records.php");
+            echo 'Post completed!';
+        }else{
+            echo 'Try again!' . $stmt->errorInfo()[2];
+        }
     }
+
+    catch(PDOException $exception){
+        die('ERROR: ' . $exception->getMessage());
+    }
+
 }
 ?>
 <!DOCTYPE html>
@@ -57,7 +63,10 @@ if($_POST){
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.3/css/all.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
@@ -276,6 +285,128 @@ a:hover {
     transform: rotate(180deg);
     color: #F8F5F2;
 }
+
+body {
+    color: #999;
+    background: #f5f5f5;
+    font-family: 'Roboto', sans-serif;
+}
+
+.form-control,
+.form-control:focus,
+.input-group-addon {
+    border-color: #e1e1e1;
+    border-radius: 0;
+    background: #F8F5F2;
+}
+
+.signup-form {
+    width: 390px;
+    margin: 0 auto;
+    padding: 30px 0;
+}
+
+.signup-form h2 {
+    color: #636363;
+    margin: 0 0 15px;
+    text-align: center;
+}
+
+.signup-form .lead {
+    font-size: 14px;
+    margin-bottom: 30px;
+    text-align: center;
+}
+
+.signup-form form {
+    border-radius: 1px;
+    margin-bottom: 15px;
+    background: #F8F5F2;
+    border: 1px solid #f3f3f3;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    padding: 30px;
+}
+
+.signup-form .form-group {
+    margin-bottom: 20px;
+}
+
+.signup-form label {
+    font-weight: normal;
+    font-size: 13px;
+}
+
+.signup-form .form-control {
+    min-height: 38px;
+    box-shadow: none !important;
+    border-width: 0 0 1px 0;
+}
+
+.signup-form .input-group-addon {
+    max-width: 42px;
+    text-align: center;
+    background: none;
+    border-bottom: 1px solid #e1e1e1;
+    padding-left: 5px;
+}
+
+.signup-form .btn,
+.signup-form .btn:active {
+    font-size: 16px;
+    color: #8593AE;
+    font-weight: bold;
+    background: #5A4E4D !important;
+    border-radius: 3px;
+    border: none;
+    min-width: 140px;
+}
+
+.signup-form .btn:hover,
+.signup-form .btn:focus {
+    background: #8593AE !important;
+    color: #5A4E4D;
+}
+
+.signup-form a {
+    color: #F8F5F2;
+    text-decoration: none;
+}
+
+.signup-form a:hover {
+    text-decoration: underline;
+}
+
+.signup-form .fa {
+    font-size: 21px;
+    position: relative;
+    top: 8px;
+}
+
+.signup-form .fa-paper-plane {
+    font-size: 17px;
+}
+
+.signup-form .fa-check {
+    color: #fff;
+    left: 9px;
+    top: 18px;
+    font-size: 7px;
+    position: absolute;
+}
+
+.register {
+    background-image: url("img/2ndslide.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+
+span {
+    background: #F8F5F2;
+}
+
+input {
+    background: #F8F5F2;
+}
 </style>
 
 <script>
@@ -294,7 +425,7 @@ $(document).ready(function() {
 </script>
 
 
-<body>
+<body class="register">
     <div class="app">
         <div id="sidenav">
             <div class="wrapper">
@@ -376,6 +507,124 @@ $(document).ready(function() {
             </div>
             <br>
         </div>
+        <div class="signup-form">
+            <form method="post">
+                <h2>Create A Report</h2>
+                <p>Create a criminal record by providing this information!</p>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-camera"></i>
+                        </span>
+                        <input type="file" class="form-control" name="mugshot" placeholder="Upload">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-user"></i>
+                        </span>
+                        <input type="text" class="form-control" name="criminal_name" placeholder="Criminal Name"
+                            required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-birthday-cake"></i>
+                        </span>
+                        <input type="date" class="form-control" name="criminal_birth_date" required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-weight"></i>
+                        </span>
+                        <input type="number" class="form-control" name="criminal_weight" placeholder="Weight (lbs)"
+                            required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-ruler-vertical"></i>
+                        </span>
+                        <input type="number" class="form-control" name="criminal_height" placeholder="Height (inches)"
+                            required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-eye"></i>
+                        </span>
+                        <input type="text" class="form-control" name="criminal_eye_color"
+                            placeholder="Criminal Eye Color" required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-user"></i>
+                        </span>
+                        <input type="text" class="form-control" name="criminal_hair_color"
+                            placeholder="Criminal Hair Color" required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-user"></i>
+                        </span>
+                        <input type="text" class="form-control" name="criminal_ethnicity"
+                            placeholder="Criminal Ethnicity" required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-gavel"></i>
+                        </span>
+                        <input type="text" class="form-control" name="criminal_charges" placeholder="Criminal Charges"
+                            required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-calendar-week"></i>
+                        </span>
+                        <input type="date" class="form-control" name="criminal_date_of_arrest" required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-flag-usa"></i>
+                        </span>
+                        <input type="text" class="form-control" name="criminal_county_of_arrest"
+                            placeholder="County of Arrest" required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-id-badge"></i>
+                        </span>
+                        <input type="text" class="form-control" name="author_of_record" placeholder="Officer"
+                            required="required">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-block btn-lg" style="background-color: #5A4E4D;">Create
+                        Record</button>
+                </div>
+
+            </form>
+        </div>
+
+
 </body>
 
 </html>
