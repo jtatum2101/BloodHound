0b9fe158ba41b48fd7ae6cf334114c0b9bf7b6e8
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-23 08:38:27 -0500

    nevermind i'll stick with mysqli.

--- config.php
+++ config.php
@@ -1,17 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname="bloodhound";
-
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=bloodhound", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'bloodhound');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
 
 
