cd5109bfbb077598c25c2b7315755a136791aba9
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-26 10:41:23 -0500

    Time to start working on the admin side of the website.

--- dashboard-admin.php
+++ dashboard-admin.php
@@ -271,22 +271,6 @@ $(document).ready(function() {
                             data-parent="#accordionExample">
                             <div class="card-body">
                                 <ul>
-                                    <li><a href="admin-profile/admin-profile.php"
-                                            style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">View
-                                            Profile</a></li>
-                                    <hr>
-                                    <li><a href="admin-profile/admin-profile-settings.php"
-                                            style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Profile
-                                            Settings</a></li>
-                                    <hr>
-                                    <li><a href="admin-profile/admin-profile-change-password.php"
-                                            style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Change
-                                            Password</a></li>
-                                    <hr>
-                                    <li><a href="admin-profile/admin-profile-recover-password.php"
-                                            style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Recover
-                                            Password</a></li>
-                                    <hr>
                                     <li><a href="logout.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Log
                                             Out</a></li>
@@ -334,25 +318,25 @@ $(document).ready(function() {
                             data-parent="#accordionExample">
                             <div class="card-body">
                                 <ul>
-                                    <li><a href="admin-records/admin-records-edit.php"
+                                    <li><a href="admin-records-edit.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Edit
                                             A Record</a></li>
                                     <hr>
-                                    <li><a href="admin-records/admin-records-view-all-records.php"
+                                    <li><a href="admin-records-view-all-records.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">View
                                             All Records</a></li>
                                     <hr>
-                                    <li><a href="admin-records/admin-records-view-by-charge.php"
+                                    <li><a href="admin-records-view-by-charge.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">View
                                             A Record By Charge</a>
                                         <hr>
                                     </li>
-                                    <li><a href="admin-records/admin-records-view-by-name.php"
+                                    <li><a href="admin-records-view-by-name.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">View
                                             A Record By Name</a>
                                         <hr>
                                     </li>
-                                    <li><a href="admin-records/admin-records-delete.php"
+                                    <li><a href="admin-records-delete.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Delete
                                             A Record</a></li>
                                     <hr>
@@ -373,19 +357,19 @@ $(document).ready(function() {
                             data-parent="#accordionExample">
                             <div class="card-body">
                                 <ul>
-                                    <li><a href="admin-counties/admin-counties-create.php"
+                                    <li><a href="admin-counties-create.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Create
                                             A County</a></li>
                                     <hr>
-                                    <li><a href="admin-counties/admin-counties-edit.php"
+                                    <li><a href="admin-counties-edit.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Edit
                                             A County</a></li>
                                     <hr>
-                                    <li><a href="admin-counties/admin-counties-delete.php"
+                                    <li><a href="admin-counties-delete.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Delete
                                             A County</a></li>
                                     <hr>
-                                    <li><a href="admin-counties/admin-counties-assign-officer-to-county.php"
+                                    <li><a href="admin-counties-assign-officer-to-county.php"
                                             style="font-size: 18px; color: #5A4E4D; font-family: 'Playfair Display', serif;">Assign
                                             An Officer to A
                                             County</a></li>
