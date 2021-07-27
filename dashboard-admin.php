e1d38e7b955d4e15d4db67d33e76d7415da7f9b1
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-27 14:25:37 -0500

    icon replaced

--- dashboard-admin.php
+++ dashboard-admin.php
@@ -11,7 +11,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BloodHound || Admin Dash</title>
-    <link rel="shortcut icon" type="image/jpg" href="img/adminlogo.jpg"/>
+    <link rel="shortcut icon" type="image/png" href="img/admin-tab-logo.png"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
