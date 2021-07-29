a16902f66c617de751635838d9f007c56b0f3e14
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-29 12:16:45 -0500

    bootstrap is not showing up on the heroku app.

--- config.php
+++ config.php
@@ -16,7 +16,7 @@ $db['default'] = array(
     'dbdriver' => 'PDO',
     'dbprefix' => '',
     'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
+    'db_debug' => (ENVIRONMENT == 'production'),
     'cache_on' => FALSE,
     'cachedir' => '',
     'char_set' => 'utf8',
