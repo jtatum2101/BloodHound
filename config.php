d1dfa7b752d20e6aff1e1959045cdf87ef25d08e
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-29 09:20:35 -0500

    deploying to heroku.

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // used to connect to the database
-$host = "localhost";
+$host = "@us-cdbr-east-04.cleardb.com";
 $db_name = "bloodhound";
-$username = "root";
-$password = "";
+$username = "be88563e567f47";
+$password = "9d62f99f";
  
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
