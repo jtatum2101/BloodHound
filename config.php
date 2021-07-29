6f811e5f011873563fe28d88c8671f64dc747713
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-29 11:59:07 -0500

    The pain that I have to go through.

--- config.php
+++ config.php
@@ -1,9 +1,34 @@
 <?php
 // used to connect to the database
-$host = "@us-cdbr-east-04.cleardb.com";
-$db_name = "heroku_e3877ac9a5772b";
-$username = "be88563e567f47";
-$password = "9d62f99f";
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn' => '',
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'PDO',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+    
+);
  
 try {
     $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
