daa858241865d37c9bb0f0b29b5a5faec6d43846
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-29 11:28:29 -0500

    config updated to use the correct db

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // used to connect to the database
 $host = "@us-cdbr-east-04.cleardb.com";
-$db_name = "bloodhound";
+$db_name = "heroku_e3877ac9a5772b";
 $username = "be88563e567f47";
 $password = "9d62f99f";
  
