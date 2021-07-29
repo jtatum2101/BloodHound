bf9665ced5839eb4cf0d8870c7613c38104e446e
Jeremiah Tatum <jtatum21@basecampcodingacademy.org>
2021-07-29 15:59:16 -0500

    favicon is acting strange but I am done styling with this project.

--- index.php
+++ index.php
@@ -26,10 +26,9 @@ if($_POST){
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="img/bloodhound.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BloodHound || Welcome!</title>
+    <link rel="icon" type= "image/x-icon" href="img/bloodhound.ico"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- register.php
+++ register.php
@@ -62,9 +62,9 @@ if($_POST){
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BloodHound | Register!</title>
-    <link rel="shortcut icon" href="img/bloodhound.ico"/>
+    <link rel="shortcut icon" href="img/bloodhound.ico">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
